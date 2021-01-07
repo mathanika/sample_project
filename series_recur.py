3429c4c338a12610fab7c24445b3e0524e23e7a9
1direction <mathanikapadmanaban@gmail.com>
2021-01-07 15:22:35 +0530

    #1(mas) This is adding a python file to create a series of no

---
+++ series_recur.py
@@ -0,0 +1,17 @@
+n=int(input("enter the value for n"))
+k=int(input("enter the value of k"))
+c=0
+def series(n,s,k,c):
+    if((s>0)and(c==0)):
+        s=s-k
+        print(s)
+        series(n,s,k,c)		
+    else:
+        c=1	
+        if(s!=n):
+            s=s+k
+            print(s)
+            series(n,s,k,c)			
+        else:
+            return 0
+series(n,n,k,c)      
