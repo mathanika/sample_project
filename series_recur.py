31ead33ca7c219162784253953087e49bd05bb23
mathanika <43029671+mathanika@users.noreply.github.com>
2021-01-07 19:01:24 +0530

    #4(ma)doing a series print

--- series_recur.py
+++ series_recur.py
@@ -1,1 +1,17 @@
-print("hello world")    
+n=50
+k=4
+c=0
+def series(n,s,k,c):
+    if((s>0)and(c==0)):
+        s=s-k
+        print(s)
+        series(n,s,k,c)		
+    else:
+        c=1	
+        if(s!=n):
+            s=s+k
+            print(s)
+            series(n,s,k,c)			
+        else:
+            return 0
+series(n,n,k,c)      
